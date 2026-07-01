6fa70029ce6e82fc58a924522f8085d0a54c6775
kidus7896 <kidus7896@gmail.com>
2026-07-01 13:34:29 -0700

    fix CORS for production

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const app = express()
 const PORT = process.env.PORT || 3001
 
 app.use(cors({
-  origin: process.env.CORS_ORIGIN || 'http://localhost:5173',
+  origin: process.env.CORS_ORIGIN || '*',
   credentials: true,
 }))
 
